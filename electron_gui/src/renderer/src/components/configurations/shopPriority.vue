4317b5d61f4c3e7b55754e447a9464756444a0d4
wyeeeee <1563031828@qq.com>
2024-06-03 17:04:08 +0800

    add shopPriority view

--- electron_gui/src/renderer/src/components/configurations/shopPriority.vue
+++ electron_gui/src/renderer/src/components/configurations/shopPriority.vue
@@ -1,9 +1,27 @@
 <script setup>
-const props = defineProps({
-    switchConfig: Object
-})
-    </script>
+import { ref, watch } from 'vue'
+import { useConfigStore } from '@renderer/stores/config'
+const configStore = useConfigStore();
+configStore.getAllConfigs().then();
+var shopList=ref([])
+if(configStore.guiConfig.server=='官服'){
+  shopList.value=configStore.staticConfig.common_shop_price_list.CN;
+}else if(configStore.guiConfig.server=='国际服'){
+  shopList.value=configStore.staticConfig.common_shop_price_list.Global;
+}else if(configStore.guiConfig.server=='B服'){
+  shopList.value=configStore.staticConfig.common_shop_price_list.CN;
+}else if(configStore.guiConfig.server=='日服'){
+  shopList.value=configStore.staticConfig.common_shop_price_list.JP;
+}
+</script>
     
-    <template>
-    <h1>asd</h1>
+<template>
+    <template v-for="items in shopList">
+    <v-checkbox class="float-left" color="primary">
+        <template v-slot:label>
+          <div>{{items[0]}}
+          </div>
+        </template>
+      </v-checkbox>
     </template>
+</template>
