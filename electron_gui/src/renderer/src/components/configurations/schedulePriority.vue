b16adb302307f9e0462f576a9b351fbdc92cb95b
wyeeeee <1563031828@qq.com>
2024-06-03 16:55:19 +0800

    add schedulePriority view

--- electron_gui/src/renderer/src/components/configurations/schedulePriority.vue
+++ electron_gui/src/renderer/src/components/configurations/schedulePriority.vue
@@ -3,6 +3,20 @@ import { ref, watch } from 'vue'
 import { useConfigStore } from '@renderer/stores/config'
 const configStore = useConfigStore();
 configStore.getAllConfigs().then();
+var regionName=[]
+if(configStore.guiConfig.server=='官服'){
+  regionName=configStore.staticConfig.lesson_region_name.CN;
+}else if(configStore.guiConfig.server=='国际服'){
+  regionName=configStore.staticConfig.lesson_region_name.Global;
+}else if(configStore.guiConfig.server=='B服'){
+  regionName=configStore.staticConfig.lesson_region_name.CN;
+}else if(configStore.guiConfig.server=='日服'){
+  regionName=configStore.staticConfig.lesson_region_name.JP;
+}
+const schedule=ref({})
+for (let i in regionName){
+  schedule.value[regionName[i]]=[false,false,false,false,1];
+}
 </script>
     
     <template>
@@ -30,35 +44,39 @@ configStore.getAllConfigs().then();
               </tr>
             </thead>
             <tbody>
-              <tr v-for="i in 10"
+              <tr v-for="(items,key) in schedule"
               >
-              <td>www</td>
+              <td>{{key}}</td>
               <td>
                 <v-checkbox-btn
+                v-model="items[0]"
                 color="primary"
                 class="pe-2">
                 </v-checkbox-btn>
             </td>
               <td>
                 <v-checkbox-btn
+                v-model="items[1]"
                 color="primary"
                 class="pe-2">
                 </v-checkbox-btn>
             </td>
               <td>
                 <v-checkbox-btn
+                v-model="items[2]"
                 color="primary"
                 class="pe-2">
                 </v-checkbox-btn>
             </td>
               <td>
                 <v-checkbox-btn
+                v-model="items[3]"
                 color="primary"
                 class="pe-2">
                 </v-checkbox-btn>
             </td>
                 <td>
-                    <v-text-field label="次数" density='compact' variant="underlined" class="pt-3"></v-text-field>
+                    <v-text-field label="次数" density='compact' variant="underlined" class="pt-3" v-model="items[4]"></v-text-field>
                 </td>
               </tr>
             </tbody>
